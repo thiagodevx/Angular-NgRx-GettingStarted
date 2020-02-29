9eb9d6ec0e11b21cba01538029be02d3c6235c90
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-29 19:47:50 -0300

    to keep the lazy loading features from angular

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,7 +1,5 @@
-import { ProductState } from './products/state/product.state';
 import { UserState } from './user/state/user.state';
 
-export interface AppState {
-  products: ProductState;
+export interface State {
   users: UserState;
 }
--- src/app/products/state/product.state.ts
+++ src/app/products/state/product.state.ts
@@ -1,7 +1,12 @@
 import { Product } from '../product';
+import * as fromRoot from '../../app.state';
 
 export interface ProductState {
   showProductCode: boolean;
   currentProduct: Product;
   products: Product[];
+}
+
+export interface State extends fromRoot.State {
+  products: ProductState;
 }
