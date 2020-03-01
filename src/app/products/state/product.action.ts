6873c1c3fbae4cf2ea0f97b37bb9ab897b500676
thiago.dev.x <thiago.dev.x@gmail.com>
2020-03-01 20:03:13 -0300

    starting to type the actions and payloads...

---
+++ src/app/products/state/product.action.ts
@@ -0,0 +1,31 @@
+import { Action } from '@ngrx/store';
+import { Product } from '../product';
+
+export enum ProductActionTypes {
+  ToogleProductCode = '[Product] Toggle Product Code',
+  SetCurrentProduct = '[Product] Set Current Product',
+  ClearCurrentProduct = '[Product] Clear Current Product',
+  InitializeCurrentProduct = '[Product] Initialize Current Product'
+}
+
+export class ToggleProductCode implements Action {
+  readonly type = ProductActionTypes.ToogleProductCode;
+  constructor(public payload: boolean) { }
+}
+
+export class SetCurrentProduct implements Action {
+  readonly type = ProductActionTypes.ToogleProductCode;
+  constructor(public payload: Product) { }
+}
+
+export class ClearCurrentProduct implements Action {
+  readonly type = ProductActionTypes.ToogleProductCode;
+  constructor(public payload: boolean) { }
+}
+
+export class InitializeCurrentProduct implements Action {
+  readonly type = ProductActionTypes.ToogleProductCode;
+  constructor(public payload: Product) { }
+}
+
+export type ProductActions = ToggleProductCode | SetCurrentProduct | ClearCurrentProduct | InitializeCurrentProduct;
